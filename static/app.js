2ee802056f0d48031d97dcd0bddf7be5dad10b20
seaskymonster <wenlaizhang@gmail.com>
2016-05-22 14:50:49 -0700

    remove unuseful code

--- static/app.js
+++ static/app.js
@@ -6,15 +6,6 @@
     ]);
 })();
 
-(function test() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('/service-worker.js')
-            .then(function () {
-                console.log('Service Worker Registered');
-            });
-    }
-})();
 
 
 
